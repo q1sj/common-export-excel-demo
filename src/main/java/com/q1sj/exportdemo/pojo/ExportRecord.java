19dfb7b27b240ea9f6fd2031d70ddd2f61643871
Q1sj <q1sj@outlook.com>
2021-05-13 21:17:22 +0800

    MyExportStatusChangeListener demo

--- src/main/java/com/q1sj/exportdemo/pojo/ExportRecord.java
+++ src/main/java/com/q1sj/exportdemo/pojo/ExportRecord.java
@@ -3,6 +3,8 @@ package com.q1sj.exportdemo.pojo;
 import com.qsj.export.AbstractExportRecord;
 
 public class ExportRecord extends AbstractExportRecord {
+    //根据业务增加字段
+
     public static ExportRecord mock(){
         ExportRecord exportRecord = new ExportRecord();
         exportRecord.setFileName("filename");

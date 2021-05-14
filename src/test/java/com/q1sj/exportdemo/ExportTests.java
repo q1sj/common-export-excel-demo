3aaa26d76e08340f2f6de701c5bd47f71e51393e
Q1sj <q1sj@outlook.com>
2021-05-14 15:16:49 +0800

    3.0.5v2

--- src/test/java/com/q1sj/exportdemo/ExportTests.java
+++ src/test/java/com/q1sj/exportdemo/ExportTests.java
@@ -1,22 +1,16 @@
 package com.q1sj.exportdemo;
 
-import com.q1sj.export.Export;
 import com.q1sj.exportdemo.task.ExportTask;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Map;
-
 @SpringBootTest
 class ExportTests {
     @Autowired
     private ExportTask exportTask;
-    @Autowired
-    private Map<String, Export> exportMap;
     @Test
     public void test(){
-//        System.out.println(exportMap);
         exportTask.run("");
     }
 }
